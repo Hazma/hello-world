b34898b9d5bbdc15fe11f4bb6b3a195082114fba
HSarwar <HSarwar-consultant@scholastic.com>
2018-05-23 23:11:14 +0500

    testing

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    # Whatever arguments you need/want
+    name='Test',
+    url='https://github.com/sarwarhamza/hello-world',
+    version='0.1',
+)
---
+++ testpackage/Test.py
@@ -0,0 +1,5 @@
+class Test:
+    def __init__(self):
+        print("initialized")
+    def test(self):
+        print("hello")
